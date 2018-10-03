a9444869c32b6f946efa883a456d1ef88433adf5
DiegoG40 <43224014+DiegoG40@users.noreply.github.com>
2018-10-03 18:57:37 -0500

    Update T3_LITROS_Ejercicio6.cpp

--- T3_LITROS_Ejercicio6.cpp
+++ T3_LITROS_Ejercicio6.cpp
@@ -4,31 +4,28 @@
 using namespace std;
 int main()
 {
-	float pago_total, litros, gasto1, gasto2, cuota_mensual;
+	float pago_total, litros, gasto1, gasto2;
 	while (1)
 	{
 		system("cls");
 		
-	cout<<"Introduzca cuota mensual: ";
-	cin>>cuota_mensual;
 	cout<<"Introduzca los litros gastados: ";
 	cin>>litros;
 	if(litros<=50)
 	{
 		gasto1=0;
-		
-	}
-	else if (litros>50 && litros <200)
-	{
-		gasto1= litros*3;
 		gasto2=0;
 	}
-	else if(litros>=200)
+	else if (litros>50 && litros <=200)
 	{
-		gasto1=litros*5;
-		
+		gasto1= 150+(litros-50)*3;
+		gasto2=0;
 	}
-	pago_total=cuota_mensual+gasto1+gasto2;
+	else if(litros>200)
+	{
+		gasto2=600+(litros-200)*5;
+	}
+	pago_total=gasto1+gasto2;
 	cout<<"El total a pagar sera: "<<pago_total<<" pesos"<<endl;
 	system("pause");
 	}
